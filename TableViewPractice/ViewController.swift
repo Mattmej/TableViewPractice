5d65e5e9fda03a14de71e90cacf25a5e104a7765
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-12-03 11:44:58 -0500

    Add local files to remote repo.

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Matt Mejia on 12/3/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
